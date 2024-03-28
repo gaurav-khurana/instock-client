686c7795e7355c7c42fae2e06f6f3d73880f29eb
Kristopher <Krrisantan@proton.me>
2024-03-28 09:19:23 -0700

    working on the Inventory Details page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Header from "./Components/Header/Header";
+import InventoryDetails from "./pages/InventoryDetails/InventoryDetails";
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
       <BrowserRouter>
       <Header />
         <Routes>
-        
-        <Route path="/*" element={<PageNotFound />} />
+
+        TODO
+        {/* <Route path="/InventoryDetails" element={<InventoryDetails />} /> */}
+        <Route path="/*" element={<InventoryDetails />} />
+        {/* <Route path="/*" element={<PageNotFound />} /> */}
+
+
         </Routes >
       </BrowserRouter>
     </div>
