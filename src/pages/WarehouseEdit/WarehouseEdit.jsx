be6d6abfa87265d8f543714e581e2cc9ed598394
Gaurav <program.drgk@gmail.com>
2024-03-28 17:24:03 -0700

    Passed state to Edit Warehouse -> Details Card & Contact Card DONE

--- src/pages/WarehouseEdit/WarehouseEdit.jsx
+++ src/pages/WarehouseEdit/WarehouseEdit.jsx
@@ -1,17 +1,24 @@
 import "./WarehouseEdit.scss";
 import BackArrow from "../../assets/Icons/arrow_back-24px.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import WarehouseDetailsCard from "../../Components/WarehouseDetailsCard/WarehouseDetailsCard";
 import WarehouseContactsCard from "../../Components/WarehouseContactsCard/WarehouseContactsCard";
 
 function WarehouseEdit() {
+  // get state from global location obj in window
+
+  const location = useLocation();
+  const singlewarehouse = location.state.singleWarehouse;
+  console.log(singlewarehouse);
+  console.log(singlewarehouse.address);
+
   return (
     <>
       <main className="warehouse-edit-main">
         <div className="warehouse-edit-main__content">
           <article className="warehouse-edit-header">
             <div className="warehouse-edit__name-container">
-              <Link>
+              <Link to={"/"}>
                 <img
                   className="warehouse-edit__image"
                   src={BackArrow}
@@ -22,8 +29,9 @@ function WarehouseEdit() {
               <h1 className="warehouse-edit__title">Edit Warehouse</h1>
             </div>
           </article>
-          <WarehouseDetailsCard />
-          <WarehouseContactsCard />
+          <WarehouseDetailsCard singleWarehouse={singlewarehouse} />
+
+          <WarehouseContactsCard singleWarehouse={singlewarehouse} />
         </div>
       </main>
     </>
