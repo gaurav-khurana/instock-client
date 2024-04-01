8d5a801effba94207325cf44bf1cbf21cdfa026c
unknown <genesisgithub@gmail.com>
2024-04-01 15:53:27 -0700

    Complete design! Just need responsiveness and event listener.

--- src/Components/WarehouseContactsCard/WarehouseContactsCard.jsx
+++ src/Components/WarehouseContactsCard/WarehouseContactsCard.jsx
@@ -7,32 +7,44 @@ function WarehouseContactsCard(singlewarehouse) {
       <div className="warehouse-edit__details">
         <h2 className="warehouse-edit__header">Contacts Details</h2>
         <form className="warehouse-edit__form">
-          <label htmlFor="warehouseContact">Contact Name</label>
+          <label htmlFor="warehouseContact" className="warehouse-contact">
+            Contact Name
+          </label>
           <input
+            className="warehouse-contact__input"
             type="text"
             id="warehouseContact"
             name="warehouseContact"
             value={singlewarehouse.singleWarehouse.contact_name}
             onSubmit={""}
           />
-          <label htmlFor="warehousePosition">Position</label>
+          <label htmlFor="warehousePosition" className="warehouse-position">
+            Position
+          </label>
           <input
+            className="warehouse-position__input"
             type="text"
             id="warehousePosition"
             name="warehousePosition"
             value={singlewarehouse.singleWarehouse.contact_position}
             onSubmit={""}
           />
-          <label htmlFor="warehousePhone">Phone Number</label>
+          <label htmlFor="warehousePhone" className="warehouse-phone">
+            Phone Number
+          </label>
           <input
+            className="warehouse-phone__input"
             type="text"
             id="warehousePhone"
             name="warehousePhone"
             value={singlewarehouse.singleWarehouse.contact_phone}
             onSubmit={""}
           />
-          <label htmlFor="warehouseEmail">Email</label>
+          <label htmlFor="warehouseEmail" className="warehouse-email">
+            Email
+          </label>
           <input
+            className="warehouse-email__input"
             type="email"
             id="warehouseEmail"
             name="warehouseEmail"
